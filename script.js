6372c427bb4d57da153a1cde2d05afd38933a7b6
Manish Kumar <149898209+manish58bit@users.noreply.github.com>
2025-02-11 20:28:03 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+var typed = new Typed(".auto-typed", {
+  strings: [
+    '"Software Engineer" ^1500',
+    '"Web Developer" ^1500',
+    '"Graphic Designer" ^1500',
+    '"Software Engineer" ^1500',
+    '"Web Developer" ^1500',
+    '"Graphic Designer" ^1500',
+    '"Software Engineer" ^1500',
+    '"Web Developer" ^1500',
+  ],
+  typeSpeed: 30,
+  backSpeed: 30,
+  looped: true,
+});
